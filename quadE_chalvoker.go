6e2736187a620b29a9198b8b8175b77b318a32ec
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-01 23:14:16 +0600

    sa

---
+++ quadE_chalvoker.go
@@ -0,0 +1,34 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func QuadE(x, y int) {
+	for vert := 0; vert < y; vert++ {
+		if vert == 0 || vert == y-1 {
+			for horiz := 0; horiz < x; horiz++ {
+				if horiz == 0 || horiz == x-1 {
+					if horiz == 0 && vert == 0 || horiz == x-1 {
+						z01.PrintRune('A')
+					} else {
+						z01.PrintRune('C')
+					}
+				} else {
+					z01.PrintRune('B')
+				}
+			}
+			z01.PrintRune('\n')
+		} else {
+			for horiz2 := 0; horiz2 < x; horiz2++ {
+				if horiz2 == 0 || horiz2 == x-1 {
+					z01.PrintRune('B')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
+func main() {
+	QuadE(5, 3)
+}
