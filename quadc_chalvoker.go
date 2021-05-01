e770233bb55b4dc0ca4277465a4952c87f71cb55
dulvoker <dulat.amirkhanov@nu.edu.kz>
2021-05-01 23:11:13 +0600

    asd

--- quadc_chalvoker.go
+++ quadc_chalvoker.go
@@ -2,7 +2,7 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func QuadB(x, y int) {
+func QuadC(x, y int) {
 	for vert := 0; vert < y; vert++ {
 		if vert == 0 || vert == y-1 {
 			for horiz := 0; horiz < x; horiz++ {
