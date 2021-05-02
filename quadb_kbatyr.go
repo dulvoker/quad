249e16ffcca31aba970c59ffb219821eb17d003b
kbatyr <someday127@gmail.com>
2021-05-02 21:26:05 +0600

    quadb_kbatyr.go

--- quadb_kbatyr.go
+++ quadb_kbatyr.go
@@ -2,10 +2,10 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func Quada(x, y int) {
+func QuadB(x, y int) {
 	if x > 0 && y > 0 {
 		z01.PrintRune(47)
-		for a := 1; a <= x-2; a++ {
+		for a := 0; a < x-2; a++ {
 			z01.PrintRune('*')
 		}
 		if x > 1 {
@@ -16,9 +16,9 @@ func Quada(x, y int) {
 		}
 	}
 	if y > 1 && x > 0 {
-		for b := 1; b <= y-2; b++ {
+		for b := 0; b < y-2; b++ {
 			z01.PrintRune('*')
-			for a := 1; a <= x-2; a++ {
+			for a := 0; a < x-2; a++ {
 				z01.PrintRune(' ')
 			}
 			if x > 1 {
@@ -32,7 +32,7 @@ func Quada(x, y int) {
 	}
 	if y > 1 && x > 0 {
 		z01.PrintRune(92)
-		for a := 1; a <= x-2; a++ {
+		for a := 0; a < x-2; a++ {
 			z01.PrintRune('*')
 		}
 		if x > 1 {
